3ce6eac7f9b5084a48cc514f009a9d5b1a2af8d5
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-01-23 14:02:00 +0300

    worked on the registration and login api ensuring the routes are functional

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const multer = require("multer");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.get('/register', (req, res) => {
+  res.end('it works!');
+});
+app.get('/login', (req, res) => {
+  res.end('it works!');
+});
+app.get('/getUser', (req, res) => {
+  res.end('it works!');
+});
+
+app.get('/upload', (req, res) => {
+  res.end('it works!');
+});
 
 app.use(express.json());
 app.use(routes);
 // Handling Errors
 app.use((err, req, res, next) => {
-    // console.log(err);
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
-    res.status(err.statusCode).json({
-      message: err.message,
-    });
+  // console.log(err);
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  res.status(err.statusCode).json({
+    message: err.message,
+  });
 });
 
-app.listen(3000,() => console.log('Server is running on port 3000'));
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
--- routes.js
+++ routes.js
@@ -1,84 +1,67 @@
-// const express = require('express');
-// const router = express.Router();
-// const db  = require('./dbConnection');
-// const { signupValidation } = require('./Validation');
-// const { validationResult } = require('express-validator');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
- 
-// router.post('/register', signupValidation, (req, res, next) => {
-//   db.query(
-//     `SELECT * FROM users WHERE LOWER(email) = LOWER(${db.escape(
-//       req.body.email
-//     )});`,
-//     (err, result) => {
-//       if (result.length) {
-//         return res.status(409).send({
-//           msg: 'This user is already in use!'
-//         });
-//       } else {
-//         // username is available
-//         bcrypt.hash(req.body.password, 10, (err, hash) => {
-//           if (err) {
-//             return res.status(500).send({
-//               msg: err
-//             });
-//           } else {
-//             // has hashed pw => add to database
-//             db.query(
-//               `INSERT INTO users (name, email, password) VALUES ('${req.body.name}', ${db.escape(
-//                 req.body.email
-//               )}, ${db.escape(hash)})`,
-//               (err, result) => {
-//                 if (err) {
-//                   throw err;
-//                   return res.status(400).send({
-//                     msg: err
-//                   });
-//                 }
-//                 return res.status(201).send({
-//                   msg: 'The user has been registerd with us!'
-//                 });
-//               }
-//             );
-//           }
-//         });
-//       }
-//     }
-//   );
-// });
- 
- 
-// module.exports = router;
-
-const router = require('express').Router();
-const {body} = require('express-validator');
-const {register} = require('./controllers/registerController');
-const {login} = require('./controllers/loginController');
-const {getUser} = require('./controllers/getUserController');
-
-router.post('/register', [
-    body('name',"The name must be of minimum 3 characters length")
-    .notEmpty()
-    .escape()
-    .trim()
-    .isLength({ min: 3 }),
-    body('email',"Invalid email address")
-    .notEmpty()
-    .escape()
-    .trim().isEmail(),
-    body('password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
-], register);
+const router = require("express").Router();
+const { body } = require("express-validator");
+const { register } = require("./controllers/registerController");
+const { login } = require("./controllers/loginController");
+const { getUser } = require("./controllers/getUserController");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 
-router.post('/login',[
-    body('email',"Invalid email address")
-    .notEmpty()
-    .escape()
-    .trim().isEmail(),
-    body('password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
-],login);
 
-router.get('/getuser',getUser);
+router.post(
+  "/register",
+  [
+    body("name", "The name must be of minimum 3 characters length")
+      .notEmpty()
+      .escape()
+      .trim()
+      .isLength({ min: 3 }),
+    body("email", "Invalid email address").notEmpty().escape().trim().isEmail(),
+    body("password", "The Password must be of minimum 4 characters length")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 4 }),
+  ],
+  register
+);
 
+router.post(
+  "/login",
+  [
+    body("email", "Invalid email address").notEmpty().escape().trim().isEmail(),
+    body("password", "The Password must be of minimum 4 characters length")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 4 }),
+  ],
+  login
+);
+
+router.get("/getuser", getUser);
+
+router.post('/upload', upload.single('image'), (req, res) => {
+    const image = req.file;
+    const name = req.body.name;
+    const type = image.mimetype;
+    const data = fs.readFileSync(image.path);
+
+    connection.query('INSERT INTO images SET ?', {
+        name: name,
+        type: type,
+        data: data
+    }, (error, results, fields) => {
+        if (error) {
+            res.send(JSON.stringify({
+                status: false,
+                message: 'Error occured: ' + error
+            }));
+        } else {
+            fs.unlinkSync(image.path);
+            res.send(JSON.stringify({
+                status: true,
+                data: results
+            }));
+        }
+    });
+});
 module.exports = router;
