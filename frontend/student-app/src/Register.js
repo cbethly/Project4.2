831d61a4872af5dcc6ee1d8fd35e568bc75f42e2
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-01-27 15:17:18 +0300

    worked on userInput

--- frontend/student-app/src/Register.js
+++ frontend/student-app/src/Register.js
@@ -1,64 +1,104 @@
-import { format } from 'mysql';
-import React, { useState, useRef, useEffect } from 'react' ;
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { format } from "mysql";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 const USER_REGEX = /^[a-zA][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-
 const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const userRef = useRef();
-    const errRef = useRef();
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [user, setUser] = useSate('');
-    const [valideName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [pwd, setPwd] = useSate('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    const [matchPwd, setMatchPwd] = useSate('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
-
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
-
-useEffect(() => {
+  useEffect(() => {
     userRef.current.focus();
-}, [])
+  }, []);
 
-
-useEffect(() => {
+  useEffect(() => {
     const result = USER_REGEX.test(user);
     console.log(result);
     console.log(user);
     setValidName(result);
-}, [user])
+  }, [user]);
 
-useEffect(() => {
+  useEffect(() => {
     const result = PWD_REGEX.test(user);
     console.log(result);
     console.log(pwd);
-    setValidPwd = pwd === matchPwd;
+    setValidPwd(result);
+    const match = pwd === matchPwd;
     setValidMatch(match);
-}, [pwd, matchPwd])
+  }, [pwd, matchPwd]);
 
-useEffect(() => {
-    setErrMsg('');
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
 
-}, [user, pwd, matchPwd])
-
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">
+          Username:
+          <span className={validName ? "valid" : "hide"}>
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
+          <span className={validName || !user ? "hide" : "invalid"}>
+            <FontAwesomeIcon icon={faTimes} />
+          </span>{" "}
+        </label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autocomplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-invalid={validName ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? "instructions" : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          4 to 24 characters.
+          <br /> Must begin with a letter.
+          <br /> letters, numbers, underscores, hyphens allowed
+        </p>
+      </form>
+    </section>
+  );
+};
 
 export default Register;
