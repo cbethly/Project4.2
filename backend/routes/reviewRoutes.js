78a6f95747fdafc0dc686a7909922c26abbafed2
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-17 09:52:02 +0300

    protecting routes and getUser

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 const {getReviews, updateReviews,setReviews,deleteReviews} = require('../controllers/reviewController');
+const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(getReviews).post(setReviews)
 router.route('/:id').put(updateReviews).delete(deleteReviews)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUser  } = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
+
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/user', getUser)
+router.get('/user', protect, getUser)
 
 
 
